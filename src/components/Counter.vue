f4841e4faeb5f1c96ec6c44ee76be1a3cf00888e
Carl97VM <barcacarlosv@gmail.com>
2023-02-21 16:34:45 -0400

    MapActions and MapGetters

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -5,16 +5,19 @@
     <h2>Computed: {{ CountComputed }}</h2>
 
     <button @click="increment">+1</button>
-    <button @click="incrementPlusFive">+5</button>
-    <button>Random</button>
+    <button @click="incrementBy">+5</button>
+    <button @click="incrementRandomInt" :disabled="isLoading">Random</button>
 
     <h1>MapState</h1>
     <h2>MapState Count: {{ count }}</h2>
     <h2>MapState lastMutation: {{ lastMutation }}</h2>
+
+    <hr>
+    <h2>Direct Getter: {{ $store.getters.squareCount }}</h2>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions, mapGetters } from 'vuex'
 
 export default {
 
@@ -22,7 +25,7 @@ export default {
         CountComputed() {
             return this.$store.state.count
         },
-        ...mapState(['count','lastMutation'])
+        ...mapState(['count','lastMutation','isLoading'])
         // ...mapState({
         //     count: state => state.count,
         //     lastMutation: state => state.lastMutation
@@ -32,9 +35,19 @@ export default {
         increment(){
             this.$store.commit('increment')
         },
-        incrementPlusFive(){
-            this.$store.commit('incrementPlusFive',5)
-        }
+        incrementBy(){
+            this.$store.commit('incrementBy',5)
+            // Este metodo puede ser llamado desde un metodo padre, asi el mapActions se ejecutara normal
+            // this.incrementRandomInt
+        },
+        incrementRandomInt(){
+            this.$store.dispatch('incrementRandonInt')
+        },
+        // ...mapActions(),
+        ...mapActions({
+            // Lo puedo nombrar como sea para llamar a la accion que quiero
+            incrementRandomInt: 'incrementRandonInt'
+        })
     }
 
 }
